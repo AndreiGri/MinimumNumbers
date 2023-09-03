ec00e11a9fe4fe44e3ebfb957ebc7b99eb2e40f2
Andrei Gridin <andreigridin918@gmail.com>
2023-09-03 19:54:50 +0500

    Добавьте файлы проекта.

---
+++ MinimumNumbers/MinimumNumbers.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    // Изменяем кодировку консоли:
+    system("chcp 1251 > nul");
+
+    int a = 0;
+    int b = 0;
+
+    cout << "Введите число a: ";
+    cin >> a;
+    cout << "Введите число b: ";
+    cin >> b;
+
+    if (a > b) {
+        cout << "Число a = " << a << " больше числа b = " << b << endl;
+    }
+    else {
+        if (a < b) {
+            cout << "Число a = " << a << " меньше числа b = " << b << endl;
+        }
+        else {
+            cout << "Числа равны!" << endl;
+        }
+    }
+
+    // Задержка окна терминала:
+    system("pause > nul");
+    return 0;
+}
